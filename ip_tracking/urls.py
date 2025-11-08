e749857263698e63f751325cc1dd2f6a796712d3
Dwayne Master 007 <thubelihledlamini88@gmail.com>
2025-11-08 11:13:28 +0200

    fix deps

---
+++ ip_tracking/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ALX IP Tracking API",
+        default_version='v1',
+        description="Milestone 6 - Security & Analytics",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
+    path('', include('ip_tracking.urls')),  # if you have one
+]
