e4b01f8c6a338096975b378d7277865a797ef7fe
Dwayne Master 007 <thubelihledlamini88@gmail.com>
2025-11-08 12:22:43 +0200

    final build.sh: system pip install

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# FORCE SYSTEM PIP — BYPASS POETRY COMPLETELY
+# SYSTEM PIP — BYPASS ALL VENVS
 python -m pip install --upgrade pip
-python -m pip install -r requirements.txt --no-cache-dir --force-reinstall
+python -m pip install -r requirements.txt --no-cache-dir --force-reinstall --no-deps
 
+# COLLECTSTATIC + MIGRATE
 python manage.py collectstatic --no-input --clear
 python manage.py migrate --no-input
